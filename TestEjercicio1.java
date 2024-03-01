74ef889fc1ae67259d945eccd97b3dd8c75e6be8
saul-isaac-gonzalez-gonzalez <161804001+saul-isaac-gonzalez-gonzalez@users.noreply.github.com>
2024-03-01 06:31:40 -0600

    Add files via upload

---
+++ TestEjercicio1.java
@@ -0,0 +1,15 @@
+
+package Ejercicio1;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class TestEjercicio1 {
+
+    public static void main(String[] args) {
+            Ejercicio1 interfaz = new Ejercicio1();
+            interfaz.setSize(300, 200);
+            interfaz.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            interfaz.setVisible(true);
+    }
+}
